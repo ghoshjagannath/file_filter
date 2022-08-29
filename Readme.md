d76d58ada389b3f6a204d7a85036bedb5e81f69b
Jagannath Ghosh <ghoshjagannath0000@gmail.com>
2022-08-29 10:41:24 +0530

    main.py added

---
+++ Readme.md
@@ -0,0 +1,4 @@
+What project you can do right now according to your skill level 
+
+1. Segregator ( jpg , png , jpeg all photo one file , pdf in one folder
+)
---
+++ main.py
@@ -0,0 +1,53 @@
+import glob 
+import os 
+import fnmatch
+import shutil
+
+# your_path=input('Type your path to search documents?------>')
+
+os.chdir(os.path.join(os.getcwd(),"file_filter"))
+
+print(os.getcwd())
+
+img_destination=os.path.join(os.getcwd(),"all_folder","photo")
+doc_destination=os.path.join(os.getcwd(),"all_folder","documents")
+
+
+
+
+        
+def image_finder():
+    file_found=0
+    img_lis=['*.png','*.jpg','*.jpeg']
+    for file in os.listdir(os.getcwd()):
+        for ext in img_lis:
+            if fnmatch.fnmatch(file,ext):
+                file_found+=1
+                shutil.copy(os.path.join(os.getcwd(),file),img_destination)
+            
+    if file_found!=0:
+        print(f"{file_found} nos file found")
+    else:
+        print("No file is found")
+            
+    
+
+def doc_finder():
+    file_found=0
+    doc_lis=['*.pdf','*.docx','*.xlsx']
+    for file in os.listdir(os.getcwd()):
+        for ext in doc_lis:
+            if fnmatch.fnmatch(file,ext):
+                file_found+=1
+                shutil.copy(os.path.join(os.getcwd(),file),doc_destination)
+            
+    if file_found!=0:
+        print(f"{file_found} nos file found")
+    else:
+        print("No file is found")
+
+
+
+if __name__=="__main__":
+    image_finder()
+    doc_finder()
